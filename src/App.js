0f440016ebb75f8ea67cfdd70f1cc084ba28f85e
Castellegeek <108758585+Castellegeek@users.noreply.github.com>
2022-10-20 15:11:39 +0200

    React State

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Style.css';
+
+class App extends React.Component {
+
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
